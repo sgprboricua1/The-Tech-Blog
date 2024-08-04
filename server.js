6ee35fd48af57ef10ab1f637098a3bcdc4438474
sgprboricua <sgprboricua@hotmail.com>
2024-08-04 13:56:53 -0400

    connected db

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log("Server Started!");
-})
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+       console.log('server started!!'); 
+       })
+   }); 
