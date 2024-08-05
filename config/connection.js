3533c3ea847dfc8d5d8affadf4e78be8ea0a47a9
sgprboricua <sgprboricua@hotmail.com>
2024-08-05 18:07:51 -0400

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,32 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.
+//     SGPR_DB, process.env.SGPR_USER, process.env.SGPR_PW,
+//     {
+//         host:'localhost',
+//         dialect: 'postgres'
+//     }
+// )
+
+// module.exports = sequelize;
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.
-    SGPR_DB, process.env.SGPR_USER, process.env.SGPR_PW,
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.SGPR_DB,
+    process.env.SGPR_USER,
+    process.env.SGPR_PW,
     {
-        host:'localhost',
-        dialect: 'postgres'
+      host: 'localhost',
+      dialect: 'postgres'
     }
-)
+  );
+}
 
 module.exports = sequelize;
